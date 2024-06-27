2245e67efc4e0e77553dad21998229cfdae36d78
NicoADominguez <98nicodominguez@gmail.com>
2024-06-27 17:05:30 -0400

    js errors

--- script.js
+++ script.js
@@ -1,26 +1,16 @@
-const form = document.getElementById('form');
-const fName = document.getElementById('fname');
-const lName = document.getElementById('lname');
-const email = document.getElementById('email');
-const queryType = document.getElementsByName('query-type');
-const message = document.getElementById('message');
-const submit = document.getElementById('submit');
+const form = document.getElementById("form");
+const fName = document.getElementById("fname");
+const lName = document.getElementById("lname");
+const email = document.getElementById("email");
+const queryType = document.getElementsByName("query-type");
+const message = document.getElementById("message");
+const submit = document.getElementById("submit");
 
-const formElements = [fName, lName, email, queryType, message];
-
-
+// const formElements = [fName, lName, email, queryType, message];
 
 form.addEventListener("submit", () => {
-    e.preventDefault(); // Prevents reload on submit
-       if (!fName) { 
-        fName.setAttribute("missing", "true");
-       }
-       
-
+  e.preventDefault(); // Prevents reload on submit
+  if (!fName.value) {
+    fName.setAttribute("missing", "true");
+  }
 });
-
-
-
-
-
-
