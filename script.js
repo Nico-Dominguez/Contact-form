969dda7b804314ddf3149a0dbca106a39ab5c9d2
NicoADominguez <98nicodominguez@gmail.com>
2024-07-01 19:44:10 -0400

    change

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-const form = document.getElementById("form");
-const fName = document.getElementById("fname");
-const lName = document.getElementById("lname");
-const email = document.getElementById("email");
-const queryType = document.getElementsByName("query-type");
-const message = document.getElementById("message");
-const submit = document.getElementById("submit");
 
-// const formElements = [fName, lName, email, queryType, message];
-
-form.addEventListener("submit", () => {
-  e.preventDefault(); // Prevents reload on submit
-  if (!fName.value) {
-    fName.setAttribute("missing", "true");
-  }
-});
